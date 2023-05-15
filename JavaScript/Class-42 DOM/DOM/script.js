63be1a83630fed72869c1a7f2a4baefce1b2bb0c
Ahmadjajja <ahmadjajja86@gmail.com>
2023-05-15 20:12:52 +0500

    DOM

---
+++ JavaScript/Class-42 DOM/DOM/script.js
@@ -0,0 +1,16 @@
+// Method 1: for accessing HTML Elements by id
+
+// let heading = document.getElementById("heading1");
+// heading.innerHTML = "Ali"
+
+// Method 2: for accessing HTML Elements by class
+
+let headings = document.getElementsByClassName("heading");
+headings[0].style.color = "red";
+headings[1].style.backgroundColor = "green"
+headings[1].style.border = "2px red solid";
+headings[1].style.padding = "4px 10px";
+
+// Will discuss types of error in programming after pray
+
+
